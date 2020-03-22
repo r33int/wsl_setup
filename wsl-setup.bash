e840c3d13646c713d7feafaeb686772b97de4e93
Ranvir Singh <ranvir@numbersandreality.com>
2020-03-22 16:07:35 +0530

    Update and rename wsl-setup.sh to wsl-setup.bash

--- wsl-setup.bash
+++ wsl-setup.bash
@@ -1,8 +1,7 @@
-#!/bin/sh
+#!/bin/bash
 WIN_USERNAME=$1
 
-# Get SSH keys from Windows environment
-# Need to make the below block more readable, basically it sets permissions for ssh-keys. 
+# Get SSH keys from Windows environment 
 umask 077
 if [ ! -d ~/.ssh ]
 then
@@ -11,40 +10,30 @@ then
   chmod 400 ~/.ssh/id*
 fi
 
-# Set less annoying permissions, this is just so I don't get annoying green bars when listing directories (Please don't judge me)
+# Setup SSH agent, umask, aliases and prompt
 echo "\n#Setting less annoying permissions\numask 077\n" >> ~/.profile
-
-# Set up passwordless sudo
-echo "echo \"$USER ALL=(ALL) NOPASSWD:ALL\" >> /etc/sudoers" | sudo bash
-
-# Setup SSH agent
 cat ./ssh-scripts >> ~/.profile
-
-# Set custom aliases to kill ssh-agent quickly
 cat ./aliases >> ~/.profile
+cat ./prompt.bash >> ~/.profile
 
-# Copy .vimrc
+# Import settings for git and vim
 cp vimrc ~/.vimrc
-# Copy .gitconfig
 cp gitconfig ~/.gitconfig
 
 ## Debian Specific stuff
-
 # Install ansible packages, for Debian like environments
 sudo apt update -y
 sudo apt upgrade -y
 sudo apt install curl ansible vim openssh-client git man-db -y
-
 # Repo clean up
 sudo apt autoremove --purge -y
 sudo apt clean all
-
 ## Debian specific stuff ends here
 
 # Let SSH not bother me everytime I connect to a new host, which happens a lot.
 sudo sed -i "s/#   StrictHostKeyChecking ask/    StrictHostKeyChecking no/g" /etc/ssh/ssh_config
-
+# Passwordless sudo
+echo "echo \"$USER ALL=(ALL) NOPASSWD:ALL\" >> /etc/sudoers" | sudo bash
 # Delete this script
 rm -rf ~/wsl-setup
-
 exit 0
